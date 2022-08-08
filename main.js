07140d7b243cb11f241daf8b254c9989d44edeac
flumile <millionmiles17@hotmail.com>
2022-08-08 14:00:20 +0700

    add search bar and adjust side edge

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 import "./style.css"
 console.log("hello from main.js")
 
+/*
 const headTripElement = document.getElementById("head-trip")
 let trip = `
 <div>hello trip</div>
 `
 headTripElement.insertAdjacentHTML("beforeend", trip)
+*/
+let test = document.getElementById("test")
+let testTwoElement = document.getElementById("test-two")
+/*
+test.addEventListener("mouseover", (event) => {
+	testTwoElement.insertAdjacentHTML("beforeend", trip)
+})
+test.onmouseover = function (event) {
+	let trip = `
+    <div>hello trip ${event.data}</div>
+    `
+	testTwoElement.insertAdjacentHTML("beforeend", trip)
+}
+*/
+
+function testHover(trip) {
+	let thisTrip = trip.getAttribute("data-trip")
+	let triptrip = `
+    <div>hello trip ${thisTrip}</div>
+    `
+	testTwoElement.insertAdjacentHTML("beforeend", triptrip)
+}
+
+test.onmouseout = function () {
+	testTwoElement.innerHTML = ""
+}
