5c96b2bdde0ce3d7170d03931996960775900928
flumile <millionmiles17@hotmail.com>
2022-08-18 14:00:18 +0700

    add carousel on popular tour packages

--- main.js
+++ main.js
@@ -81,7 +81,75 @@ articles.forEach((article) => {
 })
 
 const destinations = document.querySelectorAll(".destination")
-console.log(destinations)
 destinations.forEach((destination) => {
-	console.log(destination)
+	const img = destination.querySelector(".destination-img")
+	const text = destination.querySelector(".destination-text")
+	const tourText = text.querySelector(".destination-view-tour")
+	destination.addEventListener("mouseover", () => {
+		img.classList.add("destination-hover")
+		tourText.classList.add("view-tour")
+	})
+	destination.addEventListener("mouseout", () => {
+		img.classList.remove("destination-hover")
+		tourText.classList.remove("view-tour")
+	})
 })
+
+const packages = document.querySelectorAll(".packages")
+if (packages != null) {
+	const packageTour = document.querySelectorAll(".package")
+	let sliceIndex = 1
+	showSlice(sliceIndex)
+	setSliceBar()
+
+	function showSlice(n) {
+		console.log(n)
+		if (n > packageTour.length - 2) {
+			sliceIndex = 1
+		}
+		if (n < 1) {
+			sliceIndex = packageTour.length - 2
+		}
+		DisplayNone()
+		showPackageSet(sliceIndex - 1)
+	}
+
+	function showPackageSet(n) {
+		packageTour[n].classList.add("show-package")
+		packageTour[n + 1].classList.add("show-package")
+		packageTour[n + 2].classList.add("show-package")
+		console.log("hello")
+		packageTour.forEach((trip) => {
+			console.log(trip)
+		})
+	}
+
+	function DisplayNone() {
+		packageTour.forEach((packageSet) => {
+			packageSet.classList.remove("show-package")
+		})
+	}
+
+	function setSliceBar() {
+		const barPrev = document.getElementById("slice-bar-prev")
+		const barNext = document.getElementById("slice-bar-next")
+		barPrev.addEventListener("click", () => {
+			prevSlice(sliceIndex)
+		})
+		barNext.addEventListener("click", () => {
+			nextSlice(sliceIndex)
+		})
+	}
+
+	function prevSlice(n) {
+		sliceIndex = n - 1
+		console.log(sliceIndex)
+		showSlice(sliceIndex)
+	}
+
+	function nextSlice(n) {
+		sliceIndex = n + 1
+		console.log(sliceIndex)
+		showSlice(sliceIndex)
+	}
+}
